52c7915aeb80f8bda1d21978e20564209d467d13
tukka25 <aboodytukka@gmail.com>
2023-11-12 05:43:41 +0400

    almost there

--- mariadb/mariacon.sh
+++ mariadb/mariacon.sh
@@ -1,22 +1,12 @@
-
+#! /bin/bash
 echo "[mysqld]" >> /etc/mysql/my.cnf
 echo "bind-address=0.0.0.0" >> /etc/mysql/my.cnf
-
-# (mysqld_safe &) && sleep 3 && echo "ff"
-# mysql_install_db --datadir=/var/lib/mysql
-# sleep 10
 mysqld_safe &
 mysql_pid=$!
 sleep 10
-export USERNAME=tukka DB_NAME=wp_tuk PASSWORD=1234
+chown -R mysql:mysql $MDB_VOLUME
+# echo $DB
 mysql -u root -e "CREATE DATABASE $DB_NAME;\
-GRANT ALL ON $DB_USER.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';\
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';\
 FLUSH PRIVILEGES;"
 wait $mysql_pid
-# echo -ne '\n'
-#mariadb
-#CREATE DATABASE wp_tuk;
-#GRANT ALL ON wp_tuk.* TO 'wp_user'@'%' IDENTIFIED BY '1234';
-#FLUSH PRIVILEGES;
-#exit;
-# GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';
--- nginx/up.sh
+++ nginx/up.sh
@@ -3,5 +3,5 @@
 sudo docker network create ngin-word
 sudo docker volume create --name ngin-word-volume
 sudo docker build -t $1 .
-sudo docker run -d -p 443:443 --net ngin-word --volumes-from wd2 --name $2 $1
+sudo docker run -d -p 443:443 --net ngin-word --name $2 $1
 sudo docker exec -it $2 /bin/bash
