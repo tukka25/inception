9bd0ea2de053194e9369b92ac7a92e55cd399ff5
root <root@abdamoha.abdamoha42>
2023-11-19 08:23:15 -0500

    inshallah

--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -3,9 +3,12 @@ mkdir /run/php
 touch /run/php/php7.4-fpm.sock
 chmod -R 766 /run/php
 chown -R www-data:www-data /run/php
-# wp user create $WP_ADMIN example@coolexample.com --role=administrator
+wp core install --url=$URL --title=$WEBSITE_TITLE --admin_user=$AD_USER --admin_password=$AD_PASSWORD --admin_email=$AD_EMAIL --path=/var/www/html/wordpress --allow-root
+wp user create $WP_ADMIN $WP_EMAIL --role=administrator --user_pass=$WP_PASSWORD --path=/var/www/html/wordpress --allow-root
+wp user create $SEC_USER $SEC_EMAIL --role=author --user_pass=$SEC_PASSWORD --path=/var/www/html/wordpress --allow-root
 php-fpm7.4
 
-#wordpress 172.19.0.2 172.18.0.3
-#nginx  172.19.0.3
-#mariadb 172.18.0.2
+#wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# chmod +x wp-cli.phar
+# mv wp-cli.phar /usr/local/bin/wp
+#wp core download --allow-root
