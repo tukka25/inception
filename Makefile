10c1d4323ac040f61746274f3db992d5cdff3287
root <root@abdamoha.42.fr>
2023-11-21 19:52:16 -0500

    almost there

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
 up:
-	sudo mkdir b t
+	@if [ -d "/home/abdamoha/data" ]; then \
+		sudo rm -rf /home/abdamoha/data/mariadb \
+		sudo rm -rf /home/abdamoha/data/wordpress \
+	else \
+		echo "jala"; \
+	fi
+	sudo mkdir -p /home/abdamoha/data/mariadb
+	sudo mkdir -p /home/abdamoha/data/wordpress
 	sudo docker-compose up
-
 down:
 	sudo docker-compose down
 	sudo docker system prune --all -a -f
-	sudo rm -rf b t
-	sudo docker volume rm -f $(docker volume ls -q)
+	sudo rm -rf /home/abdamoha/data/mariadb /home/abdamoha/data/wordpress
+	sudo docker volume rm -f $$(docker volume ls -q)
+	
+clean:
+	sudo docker volume rm -f $$(docker volume ls -q)
 
 re: down up
--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
+sleep 20
 mkdir /run/php
 touch /run/php/php7.4-fpm.sock
 chmod -R 766 /run/php
 chown -R www-data:www-data /run/php
-wp core install --url=$URL --title=$WEBSITE_TITLE --admin_user=$AD_USER --admin_password=$AD_PASSWORD --admin_email=$AD_EMAIL --path=/var/www/html/wordpress --allow-root
-wp user create $WP_ADMIN $WP_EMAIL --role=administrator --user_pass=$WP_PASSWORD --path=/var/www/html/wordpress --allow-root
+cd /var/www/html/wordpress
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --force --config-file=/var/www/html/wordpress/wp-config.php --allow-root
+wp core install --url=$URL --title=$WEBSITE_TITLE --admin_user=$AD_USER --admin_password=$AD_PASSWORD --admin_email=$AD_EMAIL --path=/var/www/html/wordpress  --allow-root
 wp user create $SEC_USER $SEC_EMAIL --role=author --user_pass=$SEC_PASSWORD --path=/var/www/html/wordpress --allow-root
-php-fpm7.4
+php-fpm7.4 -F
 
 #wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 # chmod +x wp-cli.phar
