8b6558ab5315d4288b61c0badde3855a9cc38198
root <root@abdamoha.42.fr>
2023-11-22 01:36:10 -0600

    almost

--- nginx/down.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker network rm -f $2
-docker stop $1
-docker rm $1
--- nginx/up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo docker network create ngin-word
-sudo docker volume create --name ngin-word-volume
-sudo docker build -t $1 .
-sudo docker run -d -p 443:443 --net ngin-word --name $2 $1
-sudo docker exec -it $2 /bin/bash
